25b911a0c82f50b91a89d4a8f9a57760957bd028
Erick I Huacuz Guerrero <erick@hitech.com.mx>
2016-04-24 17:45:54 -0500

    Initial Commit

---
+++ Hamburguesa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hamburguesa
+//
+//  Created by Erick Huacuz Guerrero on 24/04/16.
+//  Copyright © 2016 Erick Huacuz Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
